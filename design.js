dcd7c66cbbc465b7b4b56ef4c53640df79c816de
Krushnaraj-Elango <60417684+Krushnaraj-Elango@users.noreply.github.com>
2023-07-28 15:50:18 +0530

    Add files via upload

---
+++ design.js
@@ -0,0 +1,22 @@
+ReactDOM.render(
+<div>
+    <h1>This is a REACT page.</h1>
+    <p>ReactJS is first introduced by "Jordan Walke" and now it was maintained by "FACEBOOK".</p>
+    <ul>
+        <li>Yamaha</li>
+        <li>Bajaj</li>
+        <li>Honda</li>
+        <li>TVS</li>
+        <li>Keeway</li>
+    </ul>
+    <ol>
+        <li>BMW</li>
+        <li>Audi</li>
+        <li>Suzuki</li>
+        <li>Lamborgini</li>
+        <li>Porshe</li>
+    </ol>
+    <img style={{borderRadius:"50%",marginLeft:"42%"}} src="https://img.freepik.com/free-photo/wide-angle-shot-single-tree-growing-clouded-sky-during-sunset-surrounded-by-grass_181624-22807.jpg?w=2000" height="250px" width="250px" />
+</div>
+,
+document.getElementById("content"));
